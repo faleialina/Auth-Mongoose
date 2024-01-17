7d07683aa1fbeda2b8770116a0d28d1fa6a4303d
a.falei <august3@mail.ru>
2024-01-17 22:13:02 +0300

    mainPage style

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,14 +1,17 @@
 
 import Header from '@/components/Header/Header'
-
+import FingerprintIcon from '@mui/icons-material/Fingerprint';
+import LinearProgress from '@mui/material/LinearProgress';
 import style from './page.module.css'
 
-export default function Home() {
+export default function Main() {
   return (
     <div>
       <div className={style.main} >
-        <Header/>
-        <h1>HomePage</h1>
+        <Header />
+        <h1>Main Page</h1>
+        <div><FingerprintIcon color="warning" sx={{ fontSize: 80 }}/></div>
+        <div><LinearProgress color="warning" /></div>
       </div>
     </div>
   )
