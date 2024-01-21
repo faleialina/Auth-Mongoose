4f59ee8b51a339d65b5ecaad6ece99bb26de6e53
a.falei <august3@mail.ru>
2024-01-21 17:34:58 +0300

    use swagger

--- server/src/app.js
+++ server/src/app.js
@@ -2,12 +2,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocs = require('../swagger.json');
 const user = require('./controller/user.controller');
 
 const app = express();
 app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.use('/user', user);
 app.use((error, req, res, _next) => res.send(error.message));
 
